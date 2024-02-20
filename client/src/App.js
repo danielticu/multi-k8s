29d41f0cbfc40a88e6a8934dd68df69447d5f664
danielticu <daniel.ticu97@gmail.com>
2024-02-20 13:47:06 +0200

    feat: new deployment fix

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React TEST DEPLOYMENT
+            Learn React TEST DEPLOYMENT 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
